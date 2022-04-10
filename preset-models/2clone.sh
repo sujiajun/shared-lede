81e5beae59d54e26efe0af86562a16d561c0dc92
sujiajun <dennycai@qq.com>
2022-04-10 20:56:50 +0800

    Update 2clone.sh

--- preset-models/2clone.sh
+++ preset-models/2clone.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 # 下载源码
-git clone -b master --single-branch https://github.com/Lienol/openwrt
-mv ./openwrt/* ./
+git clone https://github.com/coolsnowwolf/lede.git
+mv ./lede/* ./
 
 # 下载自定义插件
+rm -rf package/lean/luci-theme-argon
 cd package
-git clone https://github.com/jerrykuku/luci-theme-argon.git
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/jerrykuku/luci-app-argon-config.git
+git clone https://github.com/kenzok8/openwrt-packages.git
 git clone https://github.com/Ausaci/luci-app-nat6-helper.git
 git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
-git clone -b luci --single-branch https://github.com/xiaorouji/openwrt-passwall.git luci-app-passwall
