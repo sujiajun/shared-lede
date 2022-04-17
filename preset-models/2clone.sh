01a87375b22cf3a50002d68018d230dd49b9de9c
sujiajun <dennycai@qq.com>
2022-04-17 21:58:32 +0800

    Update 2clone.sh

--- preset-models/2clone.sh
+++ preset-models/2clone.sh
@@ -1,17 +1,17 @@
+
 #!/bin/sh
 # 下载源码
-git clone https://github.com/coolsnowwolf/lede.git
-mv ./lede/* ./
-
-sed -i '$a src-git kenzo https://github.com/kenzok8/small-package' feeds.conf.default
+git clone -b master --single-branch https://github.com/Lienol/openwrt
+mv ./openwrt/* ./
 
 # 下载自定义插件
-rm -rf package/lean/luci-theme-argon
-cd package
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon-18.06
+mkdir -p package/myapps
+cd package/myapps
+git clone https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/jerrykuku/luci-app-argon-config.git
-git clone https://github.com/kenzok8/openwrt-packages.git
+git clone https://github.com/jerrykuku/luci-app-vssr.git
+git clone https://github.com/jerrykuku/lua-maxminddb.git
 git clone https://github.com/Ausaci/luci-app-nat6-helper.git
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
 git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
-git clone https://github.com/ntlf9t/luci-app-easymesh.git
-git clone https://github.com/destan19/OpenAppFilter.git
+git clone -b luci --single-branch https://github.com/xiaorouji/openwrt-passwall.git luci-app-passwall
