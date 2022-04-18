6cb1a90bdadf0524c9ed6be262d924b63f9b9012
sujiajun <dennycai@qq.com>
2022-04-18 10:09:40 +0800

    Update 2modify.sh

--- preset-models/2modify.sh
+++ preset-models/2modify.sh
@@ -9,15 +9,21 @@ sed -i 's/OpenWrt/H3C-Tx1801-Plus/g' package/base-files/files/bin/config_generat
 #rm -rf package/feeds/packages/xray-core
 # 修改默认wifi名称ssid为H3C-Tx1801-Plus
 sed -i 's/ssid=OpenWrt/ssid=H3C-Tx1801-Plus/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-sed -i "/commit luci/i\uci set luci.main.mediaurlbase='/luci-static/argon'" package/lean/default-settings/files/zzz-default-settings
+#sed -i "/commit luci/i\uci set luci.main.mediaurlbase='/luci-static/argon'" package/lean/default-settings/files/zzz-default-settings
 #配置ipv6、主题
-sed -i '/exit 0/d' package/lean/default-settings/files/zzz-default-settings
-cat default-settings/config_smartdns >> package/lean/default-settings/files/zzz-default-settings
-cat default-settings/config_ipv6 >> package/lean/default-settings/files/zzz-default-settings
+#sed -i '/exit 0/d' package/lean/default-settings/files/zzz-default-settings
+#cat default-settings/config_smartdns >> package/lean/default-settings/files/zzz-default-settings
+#cat default-settings/config_ipv6 >> package/lean/default-settings/files/zzz-default-settings
 #cat default-settings/config_theme >> package/lean/default-settings/files/zzz-default-settings
-cat default-settings/config_vssr >> package/lean/default-settings/files/zzz-default-settings
-echo "exit 0" >> package/lean/default-settings/files/zzz-default-settings
+#cat default-settings/config_vssr >> package/lean/default-settings/files/zzz-default-settings
+#echo "exit 0" >> package/lean/default-settings/files/zzz-default-settings
+
+
+#复制uci-defaults脚本 
+mkdir -p files/etc/uci-defaults
+cp -f uci-scripts/* files/etc/uci-defaults
 # 版本号里显示一个自己的名字（KingKong build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 sed -i "s/OpenWrt /KingKong build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 #复制内核5.10版本CPU超频补丁
