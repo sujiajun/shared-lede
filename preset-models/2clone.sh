81914b7d73a4ee22d72d3b805f8a1a3102b6fda9
sujiajun <dennycai@qq.com>
2022-04-18 09:11:40 +0800

    Update 2clone.sh

--- preset-models/2clone.sh
+++ preset-models/2clone.sh
@@ -10,8 +10,9 @@ cd package/myapps
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/jerrykuku/luci-app-argon-config.git
 git clone https://github.com/kenzok8/openwrt-packages.git
-git clone https://github.com/Ausaci/luci-app-nat6-helper.git
+# git clone https://github.com/Ausaci/luci-app-nat6-helper.git
 git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
 git clone https://github.com/ntlf9t/luci-app-easymesh.git
 git clone https://github.com/destan19/OpenAppFilter.git
 git clone https://github.com/sirpdboy/netspeedtest.git
+git clone https://github.com/sirpdboy/luci-app-advanced.git
