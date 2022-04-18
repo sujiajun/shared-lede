526fa80c829c2ac65df04691a1f35895c3df94ca
sujiajun <dennycai@qq.com>
2022-04-18 08:52:48 +0800

    Update 2clone.sh

--- preset-models/2clone.sh
+++ preset-models/2clone.sh
@@ -1,19 +1,16 @@
-
 #!/bin/sh
 # 下载源码
-git clone -b master --single-branch https://github.com/Lienol/openwrt
-mv ./openwrt/* ./
+git clone https://github.com/coolsnowwolf/lede.git
+mv ./lede/* ./
 
 # 下载自定义插件
+rm -rf package/lean/luci-theme-argon
 mkdir -p package/myapps
 cd package/myapps
-git clone https://github.com/jerrykuku/luci-theme-argon.git
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/jerrykuku/luci-app-argon-config.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git
-git clone https://github.com/jerrykuku/lua-maxminddb.git
+git clone https://github.com/kenzok8/openwrt-packages.git
 git clone https://github.com/Ausaci/luci-app-nat6-helper.git
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
 git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
-git clone -b luci --single-branch https://github.com/xiaorouji/openwrt-passwall.git luci-app-passwall
 git clone https://github.com/ntlf9t/luci-app-easymesh.git
 git clone https://github.com/destan19/OpenAppFilter.git
