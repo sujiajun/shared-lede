ebc7e4bcf493aa48c956ba28d79afa2d4a1bde20
sujiajun <dennycai@qq.com>
2022-04-22 11:42:43 +0800

    Update 1clone.sh

--- preset-models/1clone.sh
+++ preset-models/1clone.sh
@@ -3,19 +3,26 @@
 git clone -b master --single-branch https://github.com/Lienol/openwrt
 mv ./openwrt/* ./
 
-# 下载自定义插件
-mkdir -p package/myapps
-cd package/myapps
-git clone https://github.com/jerrykuku/luci-theme-argon.git
-git clone https://github.com/jerrykuku/luci-app-argon-config.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git
-git clone https://github.com/jerrykuku/lua-maxminddb.git
-git clone https://github.com/Ausaci/luci-app-nat6-helper.git
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
-git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
-git clone -b luci --single-branch https://github.com/xiaorouji/openwrt-passwall.git luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall2.git
-git clone https://github.com/ntlf9t/luci-app-easymesh.git
-git clone https://github.com/destan19/OpenAppFilter.git
-git clone https://github.com/sirpdboy/netspeedtest.git
-git clone https://github.com/sirpdboy/luci-app-advanced.git
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git neobird https://github.com/thinktip/luci-theme-neobird.git' >>feeds.conf.default
+
+git clone https://github.com/kenzok78/luci-theme-argonne.git
